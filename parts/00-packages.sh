b428a11a0fc10626fe16412a71b6b5dbf8b28825
Steven Van Acker <steven@singularity.be>
2019-05-16 15:10:10 +0200

    more dev packages, removing GUI tools for a while

--- parts/00-packages.sh
+++ parts/00-packages.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get upgrade -y
 
 # devel tools
-apt-get install -y git build-essential maven
+apt-get install -y git vim build-essential maven ipython2 ipython3 python-pip python3-pip vim-python-jedi
 
 # net tools
 apt-get install -y wireshark nmap tcpdump minicom net-tools tcptraceroute netcat telnet tcptrace
@@ -13,9 +13,10 @@ apt-get install -y wireshark nmap tcpdump minicom net-tools tcptraceroute netcat
 apt-get install -y ansible docker.io docker-compose 
 
 # GUI stuff
-apt-get install -y gnome-tweak-tool inkscape gimp xpdf texlive-full
+#apt-get install -y gnome-tweak-tool inkscape gimp xpdf texlive-full
 
 # other
-apt-get install -y openjdk-11-jdk gnupg2 vim-python-jedi python-pip apt-transport-https p7zip-full 
+# apt-get install -y openjdk-11-jdk gnupg2 vim-python-jedi apt-transport-https p7zip-full 
+apt-get install -y gnupg2 apt-transport-https p7zip-full 
 
 
