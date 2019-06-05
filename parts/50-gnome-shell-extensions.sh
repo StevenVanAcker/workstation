498284f64bdeb45903c17cd1d5e027322a682851
Steven Van Acker <steven@singularity.be>
2019-06-05 21:10:09 +0200

    gnome shell extension

---
+++ parts/50-gnome-shell-extensions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+user=$(getent passwd 1000 | cut -d: -f1)
+
+apt-get install -y gnome-tweak-tool git
+
+# FIXME
+rm -rf ~$user/.local/share/gnome-shell/extensions ~$user/Projects.git
+
+su $user <<EOF
+mkdir -p ~/.local/share/gnome-shell/extensions
+mkdir -p ~/Projects.git
+cd ~/Projects.git
+git clone https://github.com/Shihira/gnome-extension-quicktoggler.git
+cd gnome-extension-quicktoggler/quicktoggler@shihira.github.com
+make install
+EOF
