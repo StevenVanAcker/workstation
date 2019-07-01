9503e15baebd2b394d334da376252b80d5709be0
Steven Van Acker <steven@singularity.be>
2019-07-01 21:20:54 +0200

    vscode

---
+++ parts/42-vscode.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+tf=$(tempfile)
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > $tf
+install -o root -g root -m 644 $tf /etc/apt/trusted.gpg.d/
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
+
+
+apt-get install -y apt-transport-https
+apt-get update
+apt-get install -y code # or code-insiders
+
+rm -f $tf
