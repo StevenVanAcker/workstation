06ca5986ee5b570ad2e2d1ff11be3329371961a3
Steven Van Acker <steven@singularity.be>
2019-07-02 00:13:11 +0200

    change background in case of failure

---
+++ parts/00-_temporary-background.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -e
+
+# make a backup of the good background
+cp /usr/share/backgrounds/warty-final-ubuntu.png /usr/share/backgrounds/warty-final-ubuntu.png.bak
+# copy over the temporary background
+cp ./files/usr/share/backgrounds/warty-final-ubuntu.png /usr/share/backgrounds/warty-final-ubuntu.png
+
---
+++ parts/99-restore-background.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -e
+
+# restore background
+mv /usr/share/backgrounds/warty-final-ubuntu.png.bak /usr/share/backgrounds/warty-final-ubuntu.png
+
