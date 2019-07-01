5d9287779f721832f4b5c35a2e9dea4738d15c18
Steven Van Acker <steven@singularity.be>
2019-07-01 22:06:19 +0200

    bugfix

--- parts/42-vscode.sh
+++ parts/42-vscode.sh
@@ -2,7 +2,7 @@
 
 tf=$(tempfile)
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > $tf
-install -o root -g root -m 644 $tf /etc/apt/trusted.gpg.d/
+install -o root -g root -m 644 $tf /etc/apt/trusted.gpg.d/microsoft.gpg
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
 
 
