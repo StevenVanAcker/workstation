11603c75c871c98baa0e2104e17d88604ac42e4b
Steven Van Acker <steven@singularity.be>
2019-07-08 15:49:35 +0200

    fix

--- parts/43-can.sh
+++ parts/43-can.sh
@@ -2,7 +2,7 @@
 
 apt-get install -y can-utils
 
-cat > ... <<EOF
+cat > /etc/network/interfaces.d/can0.cfg <<EOF
 auto can0
 iface can0 inet manual
         pre-up /sbin/ip link set \$IFACE type can bitrate 500000 on
