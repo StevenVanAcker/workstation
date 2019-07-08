43118f4fe6840c8d88e32f24bc3f4d56e24d144d
Steven Van Acker <steven@singularity.be>
2019-07-08 15:41:02 +0200

    can bus

---
+++ parts/43-can.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -e
+
+apt-get install -y can-utils
+
+cat > ... <<EOF
+auto can0
+iface can0 inet manual
+        pre-up /sbin/ip link set \$IFACE type can bitrate 500000 on
+        up /sbin/ifconfig \$IFACE up
+        down /sbin/ifconfig \$IFACE down
+EOF
+
