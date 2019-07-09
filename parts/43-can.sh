eb981fad7a6879f819fa57f9f73c5ad251bed844
Steven Van Acker <steven@singularity.be>
2019-07-09 09:08:21 +0200

    can update

--- parts/43-can.sh
+++ parts/43-can.sh
@@ -3,10 +3,14 @@
 apt-get install -y can-utils
 
 cat > /etc/network/interfaces.d/can0.cfg <<EOF
-auto can0
-iface can0 inet manual
-        pre-up /sbin/ip link set \$IFACE type can bitrate 500000 on
-        up /sbin/ifconfig \$IFACE up
-        down /sbin/ifconfig \$IFACE down
+allow-hotplug can0
+iface can0 can static
+    bitrate 500000
+    up /sbin/ip link set \$IFACE down
+    up /sbin/ip link set \$IFACE type can bitrate 500000
+    up /sbin/ip link set \$IFACE up
 EOF
 
+grep -q "^source-directory /etc/network/interfaces.d" /etc/network/interfaces || ( echo "source-directory /etc/network/interfaces.d" >> /etc/network/interfaces )
+
+/etc/init.d/networking reload
