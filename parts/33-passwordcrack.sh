ddfd7053baa9f66a90d87055efd72ed2ec44fa0c
Steven Van Acker <steven@singularity.be>
2019-10-02 13:30:26 +0200

    installing extra packages to make johns supportscripts work

--- parts/33-passwordcrack.sh
+++ parts/33-passwordcrack.sh
@@ -7,9 +7,15 @@ apt-get install -y hashcat
 if [ "$install_john" = "true" -a ! -e /opt/JohnTheRipper ];
 then
 	echo "==> Installing JohnTheRipper from https://github.com/magnumripper/JohnTheRipper.git"
-	apt-get -y install build-essential zlib1g-dev libssl-dev git \
+	apt-get -y install build-essential python-pip python3-pip zlib1g-dev libssl-dev git \
 		yasm libgmp-dev libpcap-dev pkg-config libbz2-dev \
-		nvidia-opencl-dev
+		nvidia-opencl-dev \
+		libcompress-raw-lzma-perl python-pyasn1 python3-pyasn1 \
+		python-protobuf python3-protobuf python-dpkt python3-dpkt \
+		libnet-pcap-perl libnetpacket-perl libnet-radius-perl \
+		python-ldap3 python3-ldap3
+	pip install plyvel parsimonious pysap
+	pip3 install plyvel parsimonious 
 	cd /opt
 	git clone https://github.com/magnumripper/JohnTheRipper.git
 	cd JohnTheRipper/src
