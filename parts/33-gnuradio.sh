3b2fec005402a9de9a8d58a4d1e319e9c18bbe6c
Steven Van Acker <steven@singularity.be>
2019-10-14 17:18:31 +0200

    gnuradio uses its own "package manager", so use that instead of ubuntus incomplete version

--- parts/33-gnuradio.sh
+++ parts/33-gnuradio.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -e
 
-apt-get install -y gnuradio
+# https://www.gnuradio.org/blog/2016-06-19-pybombs-the-what-the-how-and-the-why/
+pip install pybombs
+pybombs recipes add -f gr-recipes git+https://github.com/gnuradio/gr-recipes.git
 
+mkdir -p /opt/gnuradio
+pybombs prefix init -a default /opt/gnuradio/default/ -R gnuradio-default
