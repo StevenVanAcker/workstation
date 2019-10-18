779953ee7c7ad6058ca91946f108bb3031d6f3f8
Steven Van Acker <steven@singularity.be>
2019-10-18 15:44:39 +0200

    pybombs sucks, reverting to apt install

--- parts/33-gnuradio.sh
+++ parts/33-gnuradio.sh
@@ -1,8 +1,4 @@
 #!/bin/sh -e
 
-# https://www.gnuradio.org/blog/2016-06-19-pybombs-the-what-the-how-and-the-why/
-pip install pybombs
-pybombs recipes add -f gr-recipes git+https://github.com/gnuradio/gr-recipes.git
+apt-get install -y gnuradio gr-osmosdr
 
-mkdir -p /opt/gnuradio
-pybombs prefix init -a default /opt/gnuradio/default/ -R gnuradio-default
