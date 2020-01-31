1c9bb2dfecc9f6033493f6819a6fec01c2afa9df
Steven Van Acker <steven@singularity.be>
2020-01-31 15:01:50 +0100

    wireshark nightly

--- parts/33-wireshark.sh
+++ parts/33-wireshark.sh
@@ -4,7 +4,7 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-add-apt-repository -y ppa:wireshark-dev/stable
+add-apt-repository -y ppa:wireshark-dev/nightly
 apt update
 apt-get install -y wireshark tshark
 
