7b4fb2aa8899a6a2c50316125aa4e60f36593d9d
Steven Van Acker <steven@singularity.be>
2021-09-27 18:11:17 +0200

    notes

---
+++ parts/opt--BASE-audio-video.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#obs studio
+#pavucontrol
+#alsoftrc moving stuff:
+#	https://steamcommunity.com/app/93200/discussions/0/864959809826195633/
+#special sound routing
+#	https://obsproject.com/forum/threads/capture-single-pulseaudio-stream.35170/
+
+#vlc
+#kdenlive
