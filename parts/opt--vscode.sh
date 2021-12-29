68c807c8bd9ae6896f2eb79d83af4f836c4f1b1a
Steven Van Acker <steven@singularity.be>
2021-12-29 10:22:39 +0100

    more vscode extensions

--- parts/opt--vscode.sh
+++ parts/opt--vscode.sh
@@ -15,7 +15,28 @@ apt-get install -y apt-transport-https
 apt-get update
 apt-get install -y code # or code-insiders
 
-extensions="ms-python.python ms-vscode.cpptools eamodio.gitlens ms-azuretools.vscode-docker"
+extensions="
+	ms-python.python
+	ms-vscode.cpptools
+	eamodio.gitlens
+	ms-azuretools.vscode-docker
+	tht13.html-preview-vscode
+	james-yu.latex-workshop
+
+	ms-python.vscode-pylance
+	ms-vscode-remote.remote-containers
+
+	vscjava.vscode-java-debug
+	vscjava.vscode-java-pack
+	ms-toolsai.jupyter
+	ms-toolsai.jupyter-keymap
+	ms-toolsai.jupyter-renderers
+	redhat.java
+	vscjava.vscode-maven
+	vscjava.vscode-java-dependency
+	vscjava.vscode-java-test
+	visualstudioexptteam.vscodeintellicode
+	"
 
 for ext in $extensions;
 do
