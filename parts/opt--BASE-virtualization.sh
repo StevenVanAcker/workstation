8f684b8cf8e5a51e7e4000a1297879c5aefc74b9
Steven Van Acker <steven@singularity.be>
2022-06-23 09:11:09 +0200

    does vagrant work on jammy yet?

--- parts/opt--BASE-virtualization.sh
+++ parts/opt--BASE-virtualization.sh
@@ -7,10 +7,12 @@ export MAINUSER=$(id -nu 1000)
 echo virtualbox-ext-pack virtualbox-ext-pack/license select true | debconf-set-selections
 
 # setup terraform repo
+echo ">>> Adding terraform package repository"
 curl https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/terraform-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/terraform-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /etc/apt/sources.list.d/terraform.list
 apt-get update
 
+echo ">>> Installing packages"
 apt-get install -y 	docker.io \
 					docker-compose \
 					dosbox \
@@ -25,9 +27,11 @@ apt-get install -y 	docker.io \
 					ansible
 
 # for USB access
+echo ">>> Adding $MAINUSER to vboxusers"
 adduser $MAINUSER vboxusers
 
 # install azure-cli
+echo ">>> Installing azure-cli"
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 # Install vagrant with some extensions. Specifically, AWS integration.
@@ -37,7 +41,7 @@ apt-get install -y gnupg lsb-release software-properties-common
 ubuntuver=$(lsb_release -cs)
 echo ">>> Trying to install vagrant on Ubuntu $ubuntuver"
 
-if [ "$ubuntuver" != "jammy" ];
+if true; #[ "$ubuntuver" != "jammy" ];
 then
 	curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 	apt-add-repository -y "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
