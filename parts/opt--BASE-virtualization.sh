443e9214cf852d6bcc8ca03e5b8843e0a06d5b35
Steven Van Acker <steven@singularity.be>
2022-06-23 09:44:29 +0200

    cleanup

--- parts/opt--BASE-virtualization.sh
+++ parts/opt--BASE-virtualization.sh
@@ -41,20 +41,16 @@ apt-get install -y gnupg lsb-release software-properties-common
 ubuntuver=$(lsb_release -cs)
 echo ">>> Trying to install vagrant on Ubuntu $ubuntuver"
 
-if true; #[ "$ubuntuver" != "jammy" ];
-then
-	curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
-	apt-add-repository -y "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-	apt-get update && apt-get install -y vagrant
+curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+apt-add-repository -y "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+apt-get update && apt-get install -y vagrant
 
-	echo "==> Installing vagrant plugins"
-	# vagrant plugin install vagrant-libvirt
-	vagrant plugin install vagrant-host-shell
-	#vagrant plugin install vagrant-aws # broken on 2020-06-09 because of fricking ruby
-	vagrant plugin install vagrant-winrm
+echo "==> Installing vagrant plugins"
+# vagrant plugin install vagrant-libvirt
+vagrant plugin install vagrant-host-shell
+#vagrant plugin install vagrant-aws # broken on 2020-06-09 because of fricking ruby
+vagrant plugin install vagrant-winrm
 
-	cat > /dev/null <<EOF
+cat > /dev/null <<EOF
 ~/.vagrant.d/Vagrantfile
 EOF
-
-fi
