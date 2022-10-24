4ed03839b38d8bbeb4efb5aee29d267c4b455cb9
Steven Van Acker <steven@singularity.be>
2022-10-24 10:17:33 +0200

    check hashicorp repo exists before using it

--- parts/opt--BASE-virtualization.sh
+++ parts/opt--BASE-virtualization.sh
@@ -2,15 +2,23 @@
 # DESCRIPTION: Virtualisation tools
 
 export MAINUSER=$(id -nu 1000)
+export RELEASE=$(lsb_release -cs)
 
 # accept virtualbox-ext-pack license
 echo virtualbox-ext-pack virtualbox-ext-pack/license select true | debconf-set-selections
 
 # setup terraform repo
-echo ">>> Adding terraform package repository"
-curl https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/terraform-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/terraform-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /etc/apt/sources.list.d/terraform.list
-apt-get update
+echo ">>> Checking whether hashicorp supports release $RELEASE"
+if ! curl --output /dev/null --silent --head --fail https://apt.releases.hashicorp.com/dists/$RELEASE/Release;
+then
+	echo ">>> ... no."
+else
+	echo ">>> ... yes."
+	echo ">>> Adding terraform package repository"
+	curl https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/terraform-archive-keyring.gpg
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/terraform-archive-keyring.gpg] https://apt.releases.hashicorp.com $RELEASE main" > /etc/apt/sources.list.d/terraform.list
+	apt-get update
+fi
 
 echo ">>> Installing packages"
 apt-get install -y 	docker.io \
@@ -38,8 +46,7 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 dpkg -l vagrant && echo "==> Vagrant already installed." && exit 0
 
 apt-get install -y gnupg lsb-release software-properties-common
-ubuntuver=$(lsb_release -cs)
-echo ">>> Trying to install vagrant on Ubuntu $ubuntuver"
+echo ">>> Trying to install vagrant on Ubuntu $RELEASE"
 
 apt-get update && apt-get install -y vagrant
 
