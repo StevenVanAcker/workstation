8f65721a90528f219f362e8ca0d23388403769d9
Steven Van Acker <steven@singularity.be>
2022-11-16 08:23:56 +0100

    vscode extensions

--- parts/opt--vscode.sh
+++ parts/opt--vscode.sh
@@ -16,29 +16,45 @@ apt-get update
 apt-get install -y code # or code-insiders
 
 extensions="
-	ms-python.python
-	ms-vscode.cpptools
-	eamodio.gitlens
-	ms-azuretools.vscode-docker
-	tht13.html-preview-vscode
-	james-yu.latex-workshop
+	alex079.vscode-avr-helper
 	bierner.markdown-preview-github-styles
-	ms-vscode.cpptools-extension-pack
-
+	cschlosser.doxdocgen
+	eamodio.gitlens
+	efanzh.graphviz-preview
+	hashicorp.terraform
+	james-yu.latex-workshop
+	jeff-hykin.better-cpp-syntax
+	joaompinto.vscode-graphviz
+	josetr.cmake-language-support-vscode
+	ms-azuretools.vscode-docker
+	ms-dotnettools.vscode-dotnet-runtime
+	ms-python.python
 	ms-python.vscode-pylance
-	ms-vscode-remote.remote-containers
-
-	vscjava.vscode-java-debug
-	vscjava.vscode-java-pack
 	ms-toolsai.jupyter
 	ms-toolsai.jupyter-keymap
 	ms-toolsai.jupyter-renderers
+	ms-toolsai.vscode-jupyter-cell-tags
+	ms-toolsai.vscode-jupyter-slideshow
+	ms-vscode.cmake-tools
+	ms-vscode.cpptools
+	ms-vscode.cpptools-extension-pack
+	ms-vscode.cpptools-themes
+	ms-vscode.remote-explorer
+	ms-vscode-remote.remote-containers
+	ms-vscode-remote.remote-ssh
+	ms-vscode-remote.remote-ssh-edit
+	ms-vscode-remote.remote-wsl
 	redhat.java
-	vscjava.vscode-maven
-	vscjava.vscode-java-dependency
-	vscjava.vscode-java-test
+	tht13.html-preview-vscode
+	twxs.cmake
+	valentjn.vscode-ltex
+	visualstudioexptteam.intellicode-api-usage-examples
 	visualstudioexptteam.vscodeintellicode
-	hashicorp.terraform
+	vscjava.vscode-java-debug
+	vscjava.vscode-java-dependency
+	vscjava.vscode-java-pack
+	vscjava.vscode-java-test
+	vscjava.vscode-maven
 	"
 
 for ext in $extensions;
