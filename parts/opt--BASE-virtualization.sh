3ec86f42148d2b422fe645e9544894158d7007f5
Steven Van Acker <steven@singularity.be>
2023-10-31 18:03:59 +0100

    packer repo

--- parts/opt--BASE-virtualization.sh
+++ parts/opt--BASE-virtualization.sh
@@ -22,6 +22,17 @@ else
 	apt-get install -y terraform
 fi
 
+# install packer
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# check whether there is a repo for packer for the current ubuntu release
+if ! apt-get update;
+then
+	echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com lunar main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+	apt-get update
+fi
+
 echo ">>> Installing packages"
 apt-get install -y 	docker.io \
 					docker-compose \
