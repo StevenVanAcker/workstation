ed50a1f18739cb38be9994c3a3b64db25972c19c
Steven Van Acker <steven@singularity.be>
2024-12-30 22:34:29 +0100

    rust and cargo from source installs as root user only

--- parts/pre--010-rust-cargo.sh
+++ parts/pre--010-rust-cargo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
 # DESCRIPTION: Rust and cargo compiled from source
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
