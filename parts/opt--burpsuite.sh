70b3c285b62f87dd61f5ac7bd4ee0f8cd870f2bc
Steven Van Acker <steven@singularity.be>
2025-11-30 11:54:34 +0100

    download burpsutie from real location

--- parts/opt--burpsuite.sh
+++ parts/opt--burpsuite.sh
@@ -7,23 +7,23 @@ tmpfile=$(mktemp /tmp/burpsuite-installer.XXXXXX.sh)
 # if this script exits for whatever reason, clean up the temp file
 trap "rm -f $tmpfile" EXIT
 
-
 # Portswigger unfortunately does not provide a fixed URL for the latest version, so we have to scrape the list of all burpsuite versions
 # and pick the highest one.
 BASEURL=https://portswigger.net
 STARTURL=$BASEURL/burp/releases
 latest_path=$(curl -s $STARTURL|grep -oP "/burp/releases/professional-community-[^\"]+" | sort -nr | head -1)
 latest=$(echo $latest_path | sed 's:.*professional-community-::' | tr "-" ".")
-latest_url=$BASEURL$latest_path
-echo "==> Latest Burp Suite version is $latest at $latest_url"
+echo "==> Latest Burp Suite version is $latest"
 
-# Now select the Pro edition Linux installer that is not for arm64
-download_path=$(curl -s $latest_url|grep startdownload|grep Linux|grep Professional|grep -v arm64|grep -oP "href=[^>]*")
-download_url=$BASEURL$(echo $download_path | sed 's/href=//g')
+# Now compose the magic download string
+download_url="https://portswigger.net/burp/releases/download?product=pro&version=$latest&type=Linux"
 
 echo "==> Downloading from $download_url to $tmpfile"
 curl -L $download_url -o $tmpfile
 chmod 0755 $tmpfile
+
+s=$(sha256sum $tmpfile | awk '{print $1}')
+echo "==> Downloaded file SHA256: $s"
 
 export MAINUSER=$(id -nu 1000)
 echo "==> Installing Burp Suite as user $MAINUSER"
